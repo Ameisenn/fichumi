a45f513827c334032684fcc16fd7619d52ff19b8
Luc <luc.ameisen@yandex.com>
2019-04-09 17:02:21 +0200

    commit homework

--- fiChouMi.js
+++ fiChouMi.js
@@ -1,7 +1,9 @@
 
     //var joueur = prompt("Fais ton choix : pierre, feuille ou ciseaux");
 
-    document.getElementById("dynamic-label-input").value
+    var joueur = document.getElementById(choixjoueur);
+    //si joueur input pierre ou feuille ou ciseau et clic btn
+    //    alors le jeu se lance + affiche le resultat
 
     var random = Math.floor(Math.random() * 3 + 1);
 
@@ -14,15 +16,19 @@
         ordi = 'feuille'
     }
 
-    if (joueur === 'pierre' | joueur === 'feuille' | joueur === 'ciseaux') {
-        if (joueur === ordi) {
-            alert('resultat ' + ordi + " " + "égalité");
-        } else if (((joueur === "pierre") && (ordi === "ciseaux")) || ((joueur === "feuille") && (ordi === "pierre")) || ((joueur === "ciseaux") && (ordi === "feuille"))) {
-            alert(ordi + " " + "victoire");
-        } else {
-            alert(ordi + " " + "lose")
+
+        
+    if (joueur === 'pierre' | joueur === 'feuille' | joueur === 'ciseaux'){
+            if (joueur === ordi) {
+                alert('resultat ' + ordi + " " + "égalité");
+            } else if (((joueur === "pierre") && (ordi === "ciseaux")) || ((joueur === "feuille") && (ordi === "pierre")) || ((joueur === "ciseaux") && (ordi === "feuille"))) {
+                alert(ordi + " " + "victoire");
+            } else {
+                alert(ordi + " " + "lose")
+            }
         }
     } else {
         alert('Cette entrée n\'est pas correct')
     }
 
+    //p.onclick = function() { alert("moot!"); };
