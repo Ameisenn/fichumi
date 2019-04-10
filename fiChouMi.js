350b8fc51dd5083afff1513776ad556bfc89a216
Luc <luc.ameisen@yandex.com>
2019-04-10 18:27:56 +0200

    commit de la journée

--- fiChouMi.js
+++ fiChouMi.js
@@ -1,34 +1,46 @@
 
     //var joueur = prompt("Fais ton choix : pierre, feuille ou ciseaux");
 
-    var joueur = document.getElementById(choixjoueur);
-    //si joueur input pierre ou feuille ou ciseau et clic btn
-    //    alors le jeu se lance + affiche le resultat
+
+
+function clicResult(x) {
+    var joueur = x;
 
     var random = Math.floor(Math.random() * 3 + 1);
 
     var ordi;
+
     if (random === 1) {
         ordi = 'pierre'
+        document.createElement("img").innerHTML = choixordi;
     } else if (random === 2) {
         ordi = 'ciseaux'
+
     } else {
         ordi = 'feuille'
+
     }
 
-
-        
-    if (joueur === 'pierre' | joueur === 'feuille' | joueur === 'ciseaux'){
+    //if (joueur === 'pierre' | joueur === 'feuille' | joueur === 'ciseaux'){
             if (joueur === ordi) {
-                alert('resultat ' + ordi + " " + "égalité");
+                 result = ('resultat ' + ordi + " " + "égalité");
             } else if (((joueur === "pierre") && (ordi === "ciseaux")) || ((joueur === "feuille") && (ordi === "pierre")) || ((joueur === "ciseaux") && (ordi === "feuille"))) {
-                alert(ordi + " " + "victoire");
+                result = (ordi + " " + "victoire");
             } else {
-                alert(ordi + " " + "lose")
+                result = (ordi + " " + "lose")
             }
-        }
-    } else {
-        alert('Cette entrée n\'est pas correct')
+    //}
+    //else {
+    //    result = ('Cette entrée n\'est pas correct')
+    //}
+
+    document.getElementById("result").innerHTML = result;
+
     }
 
-    //p.onclick = function() { alert("moot!"); };
+
+
+
+
+
+
