dd69e7411043f73b3527a031370734a0066ff7ee
Luc <luc.ameisen@yandex.com>
2019-04-11 15:54:40 +0200

    chifumi fonctionnel

--- fiChouMi.js
+++ fiChouMi.js
@@ -1,63 +1,44 @@
-
-    //var joueur = prompt("Fais ton choix : pierre, feuille ou ciseaux");
-
-
-
 function clicResult(x) {
 
-    var joueur = x;
 
-    var random = Math.floor(Math.random() * 3 + 1);
+    //toute cette ligne sont toute les variables (avoir mit des virgules signifie la répétion de "var")
+    var joueur = x, // x = à la var qui lance tout la fonction
+        random = Math.floor(Math.random() * 3 + 1),
+        imgSpawn = document.getElementById("imgOrdi"), // var imgSpawn est s'incremente dans la div id imgOrdi
 
-    var imgSpawn = document.getElementById("imgOrdi");
-
-    var imgPierre = new Image(100, 200);
-    imgPierre.src = 'img/pierre2.png';
-
-    var imgCiseaux = new Image(100, 200);
-    imgCiseaux.src = 'img/ciseaux.png';
-
-    var imgFeuille = new Image(100, 200);
-    imgFeuille.src = 'img/feuille.png';
-
-    var ordi;
-    
-    if (random === 1) {
-        ordi = 'pierre';
-        imgSpawn.replaceChild(imgPierre, imgSpawn.childNodes[0]);
-
-    } else if (random === 2) {
-        ordi = 'ciseaux';
-        imgSpawn.replaceChild(imgCiseaux, imgSpawn.childNodes[0]);
-
+        ordi, // la var ordi
+        imgPierre = new Image(100, 200), // On appel une variable imgPierre qui "construit" une image 100*200
+        imgCiseaux = new Image(100, 200), // On appel une variable imgPierre qui "construit" une image 100*200
+        imgFeuille = new Image(100, 200) // On appel une variable imgPierre qui "construit" une image 100*200
+    //
+        imgPierre.src = 'img/pierre2.png' // on y met le src de l'image souhaité dans la variable imgPierre
+        imgCiseaux.src = 'img/ciseaux.png' // idem
+        imgFeuille.src = 'img/feuille.png' // idem
+    //
+    if (random === 1) {          // si var random ===1
+        ordi = 'pierre'            // var ordi = string "pierre"
+        imgSpawn.innerHTML= ""      //  var imgSpawn inject en html " rien "
+        imgSpawn.appendChild(imgPierre) // puis la var imgPierre remplace imgSpawn                ********* La je ne suis sur de capter **********
+    } else if (random === 2) {       // idem sinon si
+        ordi = 'ciseaux'
+        imgSpawn.innerHTML= ""
+        imgSpawn.appendChild(imgCiseaux)
+    } else {                            // sinon
+        ordi = 'feuille'
+        imgSpawn.innerHTML= ""
+        imgSpawn.appendChild(imgFeuille)
+    }
+    //
+    let result;                     // creation de la variable reslut
+    if (joueur === ordi) {           // si resultat strict égalite
+            result = ("Equal"); // result = "chaine de carcateur" + varOrdi pierre feuille ou ciseau + chaine de caractere
+    } else if (((joueur === "pierre") && (ordi === "ciseaux")) || ((joueur === "feuille") && (ordi === "pierre")) || ((joueur === "ciseaux") && (ordi === "feuille"))) { // different resultat possible de victoire
+        result = ("You Win"); //result = varOrdi pierre feuille ou ciseau + "chaine de caractere
     } else {
-        ordi = 'feuille';
-        imgSpawn.replaceChild(imgFeuille, imgSpawn.childNodes[0]);
-
+        result = ("You Lose") // idem + you lose
     }
-
-    //if (joueur === 'pierre' | joueur === 'feuille' | joueur === 'ciseaux'){
-            if (joueur === ordi) {
-                 result = ('resultat ' + ordi + " " + "égalité");
-            } else if (((joueur === "pierre") && (ordi === "ciseaux")) || ((joueur === "feuille") && (ordi === "pierre")) || ((joueur === "ciseaux") && (ordi === "feuille"))) {
-                result = (ordi + " " + "victoire");
-            } else {
-                result = (ordi + " " + "You lose")
-            }
-    //}
-    //else {
-    //    result = ('Cette entrée n\'est pas correct')
-    //}
-
-    document.getElementById("result").innerHTML = result;
-
-    }
-
-
-
-
-    /////////////////////////
-
+    document.getElementById("result").innerHTML= result; // inject le resulat dans le html par la div id=result.
+}
 
 
 
